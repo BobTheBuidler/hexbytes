1f6e7a599784bdc7ac2c5114e7bc42877341e2f2
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-09-30 13:17:19 -0400

    fix: set allow_interpreted_subclasses=True

--- faster_hexbytes/main.py
+++ faster_hexbytes/main.py
@@ -33,7 +33,10 @@ BytesLike = Union[bytes, str, bool, bytearray, int, memoryview]
 _bytes_new: Final = bytes.__new__
 
 
-@mypyc_attr(native_class=False)
+@mypyc_attr(
+    native_class=False,
+    allow_interpreted_subclasses=True,
+)
 class HexBytes(hexbytes.HexBytes):
     """
     Thin wrapper around the python built-in :class:`bytes` class.
